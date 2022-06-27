d72d5f9d5a1ada19263de274b4794d98358fff8a
Vishesh <vishesht27@gmail.com>
2022-06-27 13:29:16 +0530

    Updated files and pre-buil-model

--- Smart-WebCam/script.js
+++ Smart-WebCam/script.js
@@ -1,23 +1,27 @@
-const status = document.getElementById("status");
-status.innerText = "Loaded TensorFlow.js - version: " + tf.version.tfjs;
+const status = document.getElementById('status');
+if (status) {
+  status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
+}
 
-const video = document.getElementById("webcam");
-const liveView = document.getElementById("liveView");
-const demosSection = document.getElementById("demos");
-const enableWebcamButton = document.getElementById("webcamButton");
+const video = document.getElementById('webcam');
+const liveView = document.getElementById('liveView');
+const demosSection = document.getElementById('demos');
+const enableWebcamButton = document.getElementById('webcamButton');
+
 
 // Check if webcam access is supported.
 function getUserMediaSupported() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
 }
 
 // If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will
+// wants to activate it to call enableCam function which we will 
 // define in the next step.
 if (getUserMediaSupported()) {
-  enableWebcamButton.addEventListener("click", enableCam);
+  enableWebcamButton.addEventListener('click', enableCam);
 } else {
-  console.warn("getUserMedia() is not supported by your browser");
+  console.warn('getUserMedia() is not supported by your browser');
 }
 
 // Enable the live webcam view and start classification.
@@ -26,19 +30,19 @@ function enableCam(event) {
   if (!model) {
     return;
   }
-
+  
   // Hide the button once clicked.
-  event.target.classList.add("removed");
-
+  event.target.classList.add('removed');  
+  
   // getUsermedia parameters to force video but not audio.
   const constraints = {
-    video: true,
+    video: true
   };
 
   // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
     video.srcObject = stream;
-    video.addEventListener("loadeddata", predictWebcam);
+    video.addEventListener('loadeddata', predictWebcam);
   });
 }
 
@@ -48,14 +52,14 @@ function enableCam(event) {
 var model = undefined;
 
 // Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment
+// loading. Machine Learning models can be large and take a moment 
 // to get everything needed to run.
 // Note: cocoSsd is an external object loaded from our index.html
 // script tag import so ignore any warning in Glitch.
 cocoSsd.load().then(function (loadedModel) {
   model = loadedModel;
   // Show demo section now model is ready to use.
-  demosSection.classList.remove("invisible");
+  demosSection.classList.remove('invisible');
 });
 
 var children = [];
@@ -68,39 +72,26 @@ function predictWebcam() {
       liveView.removeChild(children[i]);
     }
     children.splice(0);
-
+    
     // Now lets loop through predictions and draw them to the live view if
     // they have a high confidence score.
     for (let n = 0; n < predictions.length; n++) {
       // If we are over 66% sure we are sure we classified it right, draw it!
       if (predictions[n].score > 0.66) {
-        const p = document.createElement("p");
-        p.innerText =
-          predictions[n].class +
-          " - with " +
-          Math.round(parseFloat(predictions[n].score) * 100) +
-          "% confidence.";
-        p.style =
-          "margin-left: " +
-          predictions[n].bbox[0] +
-          "px; margin-top: " +
-          (predictions[n].bbox[1] - 10) +
-          "px; width: " +
-          (predictions[n].bbox[2] - 10) +
-          "px; top: 0; left: 0;";
+        const p = document.createElement('p');
+        p.innerText = predictions[n].class  + ' - with ' 
+            + Math.round(parseFloat(predictions[n].score) * 100) 
+            + '% confidence.';
+        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
+            + (predictions[n].bbox[1] - 10) + 'px; width: ' 
+            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
 
-        const highlighter = document.createElement("div");
-        highlighter.setAttribute("class", "highlighter");
-        highlighter.style =
-          "left: " +
-          predictions[n].bbox[0] +
-          "px; top: " +
-          predictions[n].bbox[1] +
-          "px; width: " +
-          predictions[n].bbox[2] +
-          "px; height: " +
-          predictions[n].bbox[3] +
-          "px;";
+        const highlighter = document.createElement('div');
+        highlighter.setAttribute('class', 'highlighter');
+        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+            + predictions[n].bbox[1] + 'px; width: ' 
+            + predictions[n].bbox[2] + 'px; height: '
+            + predictions[n].bbox[3] + 'px;';
 
         liveView.appendChild(highlighter);
         liveView.appendChild(p);
@@ -108,7 +99,7 @@ function predictWebcam() {
         children.push(p);
       }
     }
-
+    
     // Call this function again to keep predicting when the browser is ready.
     window.requestAnimationFrame(predictWebcam);
   });
